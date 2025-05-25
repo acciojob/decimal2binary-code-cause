e44b8983cde4f1fd9bad4952fdb22e2abe6000d2
Ashish Pandey <writeashish22@gmail.com>
2025-05-25 22:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function decimalToBinary(num) {
+function decimalToBinary(n) {
   //Write you code here
-  
+   let binary = "";
+  if (n === 0) {
+    return "0";
+  }
+
+  while (n > 0) {
+    binary = (n % 2) + binary;
+    n = Math.floor(n / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
